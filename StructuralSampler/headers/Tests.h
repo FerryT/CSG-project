743d3d2afbc5aab7baccc35cb25adf8040d4cfab
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-05-11 12:29:53 +0200

    Implemented QualityTest

--- StructuralSampler/headers/Tests.h
+++ StructuralSampler/headers/Tests.h
@@ -1,6 +1,7 @@
 #ifndef TESTS_H
 #define TESTS_H
 
+#include "Graph.h"
 #include "Algorithm.h"
 #include "GraphInput.h"
 
@@ -17,6 +18,26 @@ class QualityTest: Test {
 public:
 	int SnapshotSize = 10000;
 	virtual void RunTest(string outputFile) override;
+};
+
+class QualityTestCapture : Algorithm
+{
+	graph* _currentGraph;
+public:
+	Algorithm* RealAlgorithm;
+
+	QualityTestCapture();
+	~QualityTestCapture();
+	
+	virtual void Add(edge e) override;
+	virtual void Remove(edge e) override;
+
+	//The query methods:
+	virtual int FindClusterIndex(vertex u) override;
+	virtual vector<vertex> FindCluster(vertex u) override;
+	virtual int CountClusters() override;
+
+	graph* GetCompleteGraph();
 };
 
 /*
--- StructuralSampler/src/Tests.cpp
+++ StructuralSampler/src/Tests.cpp
@@ -1,15 +1,90 @@
+#include <iostream>
+#include <fstream>
+
 #include "../headers/Tests.h"
 
-void QualityTest::RunTest(string outputFile)
+using namespace std;
+
+void QualityTest::RunTest(string outputFilename)
 {
+	ofstream output;
+
+	output.open(outputFilename, ios::out);
+
+	output << "updates,cutsize" << endl;
+
+	//todo set the output algorithm
+	QualityTestCapture* capture = new QualityTestCapture();
+	capture->RealAlgorithm = this->algorithm;
+
+	int updates = 0;
+
 	while (!this->input->IsEnd())
 	{
 		for (int i = 0; !this->input->IsEnd() && i < this->SnapshotSize; i++)
 		{
 			this->input->ExecuteNextUpdate();
+			updates++;
 		}
 
+		graph* g = capture->GetCompleteGraph();
+
+		int cutSize = 0;
+		for (edge edge : *g)
+		{
+			vertex c1 = this->algorithm->FindClusterIndex(edge.v1);
+			vertex c2 = this->algorithm->FindClusterIndex(edge.v2);
+			if (c1 != c2)
+				cutSize++;
+		}
+
+		output << updates << "," << cutSize << endl;
 
 	}
 
+	output.close();
+
+}
+
+QualityTestCapture::QualityTestCapture()
+{
+	this->_currentGraph = new graph();
+}
+
+QualityTestCapture::~QualityTestCapture()
+{
+	delete this->_currentGraph;
+}
+
+void QualityTestCapture::Add(edge e)
+{
+	this->RealAlgorithm->Add(e);
+	this->_currentGraph->push_back(e);
+}
+
+int QualityTestCapture::FindClusterIndex(vertex u)
+{
+	return this->RealAlgorithm->FindClusterIndex(u);
+}
+
+vector<vertex> QualityTestCapture::FindCluster(vertex u)
+{
+	return this->RealAlgorithm->FindCluster(u);
+}
+
+int QualityTestCapture::CountClusters()
+{
+	return this->RealAlgorithm->CountClusters();
+}
+
+graph* QualityTestCapture::GetCompleteGraph()
+{
+	return this->_currentGraph;
+}
+
+void QualityTestCapture::Remove(edge e)
+{
+	this->RealAlgorithm->Remove(e);
+	graph::iterator it = find(this->_currentGraph->begin(), this->_currentGraph->end(), e);
+	this->_currentGraph->erase(it);
 }
