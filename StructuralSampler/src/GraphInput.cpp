3113a2a54d25feb837b4c091ae1681dbf9d272ad
FerryT <f.timmers@student.tue.nl>
2015-05-11 13:33:32 +0200

    initial version of graph file reading

--- StructuralSampler/src/GraphInput.cpp
+++ StructuralSampler/src/GraphInput.cpp
@@ -2,10 +2,32 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <cctype>
+#include <cstring>
 
 #include "GraphInput.h"
 
 #define BUFFER_SIZE 4096
+
+//------------------------------------------------------------------------------
+
+char *read_line(char *str, int num, FILE *fp)
+{
+	char *ret = fgets(str, num, fp);
+	if (!ret)
+		return NULL;
+	
+	size_t len = strlen(str);
+	if (!feof(fp) && str[len - 1] != '\n')
+		throw "Buffer overflow whilst reading graph file.";
+	
+	while (*str && isblank(*str))
+		++str;
+	
+	if (*str == '%')
+		return read_line(str, num, fp);
+	return str;
+}
 
 //------------------------------------------------------------------------------
 
@@ -14,6 +36,15 @@ struct FileInput::File
 	FILE *fp;
 	char *buffer;
 	char *ptr;
+	
+	// Header info
+	size_t num_nodes;
+	size_t num_edges;
+	unsigned short format;
+	unsigned short num_cons;
+	
+	// Number of lines read
+	size_t index;
 };
 
 //------------------------------------------------------------------------------
@@ -27,6 +58,25 @@ void FileInput::Open()
 	file->fp = fopen(filename.c_str(), "rt");
 	file->buffer = new char[BUFFER_SIZE];
 	file->ptr = file->buffer;
+	file->index = 0;
+	
+	if (!read_line(file->buffer, BUFFER_SIZE, file->fp))
+	{
+		Close();
+		return;
+	}
+	
+	// Parse header
+	file->num_nodes = strtoul(file->ptr, &file->ptr, 10);
+	file->num_edges = strtoul(file->ptr, &file->ptr, 10);
+	file->format = (unsigned short) strtoul(file->ptr, &file->ptr, 2);
+	file->num_cons = (unsigned short) strtoul(file->ptr, &file->ptr, 10);
+	if (!file->num_cons)
+		file->num_cons = 1;
+	
+	// Reset buffer
+	file->ptr = file->buffer;
+	*file->ptr = 0;
 }
 
 //------------------------------------------------------------------------------
@@ -43,14 +93,51 @@ void FileInput::Close()
 }
 
 //------------------------------------------------------------------------------
-
+// Todo: support multiple formats
 void FileInput::ExecuteNextUpdate()
 {
+	if (!file)
+		return;
+	
+	// Scan for next digit or end of line;
+	while (isspace(*file->ptr))
+		++file->ptr;
+	
+	// End of line, try the next one
+	if (*file->ptr == '\0')
+	{
+		if (file->index == file->num_nodes)
+		{
+			// Number of nodes specified met
+			Close();
+			return;
+		}
+		if (!read_line(file->buffer, BUFFER_SIZE, file->fp))
+		{
+			// End of input
+			Close();
+			return;
+		}
+		++file->index;
+		file->ptr = file->buffer;
+	}
+	
+	// Read next destination node
+	size_t dest = strtoul(file->ptr, &file->ptr, 10);
+	if (dest)
+	{
+		// Todo: output (file->index,dest) as an edge.
+		printf("%d %d\n", file->index, dest); // debug
+	}
+	else // Probably hit end of line, try again
+		FileInput::ExecuteNextUpdate();
 }
 
 //------------------------------------------------------------------------------
+
 bool FileInput::IsEnd()
 {
+	return !file;
 }
 
 //------------------------------------------------------------------------------
