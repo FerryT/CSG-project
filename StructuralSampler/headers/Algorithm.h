726a887ae1667a26593336a584cbcdbc12c56152
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-05-20 13:29:46 +0200

    Added a query that gets a cluster based on index

--- StructuralSampler/headers/Algorithm.h
+++ StructuralSampler/headers/Algorithm.h
@@ -17,6 +17,8 @@ class Algorithm: public Output {
 		virtual vector<vertex> FindCluster(vertex u) = 0;
 		// returns the number of clusters
 		virtual int CountClusters() = 0;
+		// gets all vertices of cluster
+		virtual vector<vertex> GetCluster(int index) = 0;
 };
 
 //------------------------------------------------------------------------------
