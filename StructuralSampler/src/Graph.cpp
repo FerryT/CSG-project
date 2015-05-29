de3df3738c1c2eee6bf6e623a098cdbc74281dfb
FerryT <f.timmers@student.tue.nl>
2015-05-29 15:39:20 +0200

    cleaned up Edge constructor

--- StructuralSampler/src/Graph.cpp
+++ StructuralSampler/src/Graph.cpp
@@ -4,13 +4,7 @@
 
 //------------------------------------------------------------------------------
 
-Edge::Edge(vertex u, vertex v) : v1(u), v2(v)
-{
-	if (u < v)
-		{ v1 = u; v2 = v; }
-	else
-		{ v1 = v; v2 = u; }
-	p = (double) (rand()) / (double) (RAND_MAX);
-}
+Edge::Edge(vertex u, vertex v) : v1(u < v ? u : v), v2(u < v ? v : u),
+	p((double)(rand()) / (double)(RAND_MAX)) { }
 
 //------------------------------------------------------------------------------
