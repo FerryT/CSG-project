c179c8ccc342ac862017de2c0c1a5c18f116f129
FerryT <f.timmers@student.tue.nl>
2015-05-31 18:26:21 +0200

    completed new graph manager implementation (still untested)

--- StructuralSampler/src/GraphManager.cpp
+++ StructuralSampler/src/GraphManager.cpp
@@ -38,9 +38,18 @@ class Cluster
 		{
 			return find()->count;
 		}
-		bool IsRoot()
+		bool IsRoot() const
 		{
 			return parent == this;
+		}
+		void Reset()
+		{
+			count = 1;
+			if (parent != this)
+			{
+				(*pool)[this] = pool->size();
+				parent = this;
+			}
 		}
 	private:
 		Cluster *parent;
@@ -82,19 +91,71 @@ struct GraphManager::Data
 {
 	ClusterPool pool;
 	ClusterIDPool ids;
-
-	void AddCluster(const vertex &v);
-	Vertices RemoveCluster(const vertex &v);
-	bool InCluster(const vertex &v) const;
-	void MergeCluster(const vertex &v1, const vertex &v2);
-	
 	Graph graph;
 	Less2Graph l2graph;
+
+	void AddVertex(const vertex &v);
+	void RemoveVertex(const vertex &v);
+	bool HasVertex(const vertex &v) const;
+	
+	void Merge(const Edge &e);
 	
 	void AddEdge(const Edge &e);
 	void RemoveEdge(const Edge &e);
-	Edges FindEdges(const vertex &v);
+	Edges FindEdges(const vertex &v) const;
+	bool HasEdgesWith(const vertex &v) const;
 };
+
+//------------------------------------------------------------------------------
+
+void GraphManager::Data::AddVertex(const vertex &v)
+{
+	pool[v] = new Cluster(&ids);
+}
+
+//------------------------------------------------------------------------------
+
+void GraphManager::Data::RemoveVertex(const vertex &v)
+{
+	Vertices vs; // Holds vertices belonging to resolved cluster
+	
+	Cluster *c = pool[v];
+	clusterid cid = *c;
+	vs.reserve(c->Count() - 1);
+	pool.erase(v); // Remove vertex
+	
+	// Resolve cluster vertex belonged to
+	for (ClusterPool::iterator it = pool.begin(); it != pool.end(); ++it)
+	{
+		if (*it->second == cid)
+		{
+			it->second->Reset();
+			vs.push_back(it->first);
+		}
+	}
+	
+	// Free removed cluster: now since it could have been a cluster root
+	delete c;
+	
+	// Recluster
+	for (const vertex &v : vs)
+		for (const Edge &e : FindEdges(v))
+			Merge(e);
+}
+
+//------------------------------------------------------------------------------
+
+bool GraphManager::Data::HasVertex(const vertex &v) const
+{
+	return (pool.count(v) == 1);
+}
+
+//------------------------------------------------------------------------------
+
+void GraphManager::Data::Merge(const Edge &e)
+{
+	(*pool[e.v1]) += pool[e.v2];
+}
 
 //------------------------------------------------------------------------------
 
@@ -114,7 +175,7 @@ void GraphManager::Data::RemoveEdge(const Edge &e)
 
 //------------------------------------------------------------------------------
 
-Edges GraphManager::Data::FindEdges(const vertex &v)
+Edges GraphManager::Data::FindEdges(const vertex &v) const
 {
 	Edges es;
 	
@@ -139,55 +200,15 @@ Edges GraphManager::Data::FindEdges(const vertex &v)
 
 //------------------------------------------------------------------------------
 
-void GraphManager::Data::AddCluster(const vertex &v)
+bool GraphManager::Data::HasEdgesWith(const vertex &v) const
 {
-	pool[v] = new Cluster(&ids);
-}
-
-//------------------------------------------------------------------------------
-
-Vertices GraphManager::Data::RemoveCluster(const vertex &v)
-{
-	Vertices vs;
-	Cluster *c = pool[v];
-	clusterid cid = (clusterid) *c;
-	vs.reserve(c->Count());
+	if (graph.lower_bound(Edge(v, 0)) != graph.lower_bound(Edge(v + 1, 0)))
+		return true;
 	
-	Cluster *root = NULL;
-	for (ClusterPool::iterator it = pool.begin(); it != pool.end();)
-	{
-		if ((clusterid) (*it->second) == cid)
-		{
-			if (it->second->IsRoot())
-				root = it->second;
-			else
-				delete it->second;
-			pool.erase(it);
-			vs.push_back(it->first);
-		}
-		else
-		{
-			it++;
-		}
-	}
-	if (root)
-		delete root;
+	if (l2graph.lower_bound(Edge(0, v)) != l2graph.lower_bound(Edge(0, v + 1)))
+		return true;
 	
-	return vs;
-}
-
-//------------------------------------------------------------------------------
-
-bool GraphManager::Data::InCluster(const vertex &v) const
-{
-	return (pool.count(v) == 1);
-}
-
-//------------------------------------------------------------------------------
-
-void GraphManager::Data::MergeCluster(const vertex &v1, const vertex &v2)
-{
-	(*pool[v1]) += pool[v2];
+	return false;
 }
 
 //==============================================================================
@@ -213,228 +234,30 @@ GraphManager::~GraphManager()
 }
 
 //------------------------------------------------------------------------------
-/*
-void GraphManager::MergeClusters(int startCluster)
-{
-	std::vector<vector<vertex>>::size_type i = startCluster;
-	while (i < clusters.size())
-	{
-		vector<vertex> currentCluster = clusters[i];
-		vector<vertex> currentClusterConnections;
-		int mergeWith = -1;
-		for (vector<vertex>::size_type j = 0; j < currentCluster.size(); j++)
-		{
-			for (pair<int,int> k : graph.at(clusters[i][j]))
-			{
-				if (k.second>0)
-				{
-					int clusterOfk = FindClusterIndex(k.first);
-					if (clusterOfk != i)
-					{
-						mergeWith = clusterOfk;
-					}
-					if (mergeWith >= 0)
-					{
-						break;
-					}
-				}
-			}
-			if (mergeWith >= 0)
-			{
-				break;
-			}
-		}
-		
-		if (mergeWith >= 0)
-		{
-			UnionClusters(i, mergeWith);
-
-			if (mergeWith < i)
-			{
-				i--;
-			}
-		}
-		else
-		{
-			i++;
-		}
-	}
-}
-
-//------------------------------------------------------------------------------
-
-void GraphManager::RemakeClusters(int c1, int c2)
-{
-	vector<vertex> theCluster1(clusters[c1]);
-	vector<vertex> theCluster2(clusters[c2]);
-	bool sameCluster = c1 == c2;
-
-	if (c1 > c2)
-	{
-		clusters.erase(clusters.begin() + c1);
-		clusters.erase(clusters.begin() + c2);
-	}
-	else if (!sameCluster)
-	{
-		clusters.erase(clusters.begin() + c2);
-		clusters.erase(clusters.begin() + c1);
-	}
-	else
-	{
-		clusters.erase(clusters.begin() + c1);
-	}
-
-	for (std::vector<vertex>::size_type i = 0; i != theCluster1.size(); i++)
-	{
-		vector<vertex> newCluster;
-		newCluster.push_back(theCluster1[i]);
-		clusters.push_back(newCluster);
-	}
-	if (!sameCluster)
-	{
-		for (std::vector<vertex>::size_type i = 0; i != theCluster2.size(); i++)
-		{
-			vector<vertex> newCluster;
-			newCluster.push_back(theCluster2[i]);
-			clusters.push_back(newCluster);
-		}
-	}
-
-	int startindex = clusters.size()-theCluster1.size();
-	if (!sameCluster)
-	{
-		startindex -= theCluster2.size();
-	}
-	MergeClusters(startindex);
-}
-
-//------------------------------------------------------------------------------
-
-void GraphManager::UnionClusters(int c1, int c2)
-{
-	if (c1 == c2)
-	{
-		return;
-	}
-	vector<vertex> newCluster;
-	newCluster.reserve(clusters[c1].size() + clusters[c2].size());
-	newCluster.insert(newCluster.end(), clusters[c1].begin(), clusters[c1].end());
-	newCluster.insert(newCluster.end(), clusters[c2].begin(), clusters[c2].end());
-
-	if (c1 > c2)
-	{
-		clusters.erase(clusters.begin() + c1);
-		clusters.erase(clusters.begin() + c2);
-	}
-	else
-	{
-		clusters.erase(clusters.begin() + c2);
-		clusters.erase(clusters.begin() + c1);
-	}
-
-	clusters.push_back(newCluster);
-}
-
-//------------------------------------------------------------------------------
-
-void GraphManager::AddEdgeToGraph(const Edge &e)
-{
-	if (graph.count(e.v1) == 0)
-	{
-		graph.insert({ e.v1, {} });
-	}
-
-	if (graph.count(e.v2) == 0)
-	{
-		graph.insert({ e.v2, {} });
-	}
-
-	if (graph.at(e.v1).count(e.v2) == 0)
-	{
-		graph.at(e.v1).insert({ e.v2, 1 });
-	}
-	else
-	{
-		graph.at(e.v1).at(e.v2) += 1;
-	}
-
-	if (graph.at(e.v2).count(e.v1) == 0)
-	{
-		graph.at(e.v2).insert({ e.v1, 1 });
-	}
-	else
-	{
-		graph.at(e.v2).at(e.v1) += 1;
-	}
-}
-
-//------------------------------------------------------------------------------
-
-void GraphManager::removeFromCluster(int v)
-{
-	vector< vector<vertex> >::size_type cluster = -1;
-	vector<vertex>::size_type location = -1;
-	for (std::vector< vector<vertex> >::size_type i = 0; i != clusters.size(); i++)
-	{
-		for (std::vector<vertex>::size_type j = 0; j != clusters[i].size(); j++)
-		{
-			if (clusters[i][j] == v)
-			{
-				cluster = i;
-				location = j;
-				break;
-			}
-		}
-		if (cluster != -1)
-		{
-			break;
-		}
-	}
-	clusters.at(cluster).erase(clusters.at(cluster).begin() + location);
-	if (clusters.at(cluster).size() == 0)
-	{
-		clusters.erase(clusters.begin() + cluster);
-	}
-}
-*/
-//------------------------------------------------------------------------------
 
 void GraphManager::Add(const Edge &e)
 {
-	/*
-	//update theGraph
-	AddEdgeToGraph(e);
-	int cluster_v1 = FindClusterIndex(e.v1);
-
-	//update theClusters
-	if (cluster_v1<0)
-	{
-		vector<vertex> newCluster;
-		newCluster.push_back(e.v1);
-		clusters.push_back(newCluster);
-		cluster_v1 = clusters.size() - 1;
-	}
-
-	int cluster_v2 = FindClusterIndex(e.v2);
-	if (cluster_v2<0)
-	{
-		vector<vertex> newCluster;
-		newCluster.push_back(e.v2);
-		clusters.push_back(newCluster);
-		cluster_v2 = clusters.size() - 1;
-	}
-
-	if (cluster_v1 != cluster_v2)
-	{
-		UnionClusters(cluster_v1, cluster_v2);
-	}
-	*/
+	// Adds an edge to the graph
+	// When an edge contains non-existing verices, add them to the pool
+	// Merge whatever clusters are joined by this edge
+	// (When the clusters are already joined nothing will happen)
+	
+	if (!data->HasVertex(e.v1))
+		data->AddVertex(e.v1);
+	
+	if (!data->HasVertex(e.v2))
+		data->AddVertex(e.v2);
+	
+	data->Merge(e);
+	data->AddEdge(e);
 }
 
 //------------------------------------------------------------------------------
 
 void GraphManager::RemoveExact(const Edge &e)
 {
+	// We assume only one edge can exist between two nodes:
+	// The normal remove method is already exact
 	Remove(e);
 }
 
@@ -442,63 +265,19 @@ void GraphManager::RemoveExact(const Edge &e)
 
 void GraphManager::Remove(const Edge &e)
 {
-	/*
-	graph.at(e.v1).at(e.v2) -= 1;
-	graph.at(e.v2).at(e.v1) -= 1;
-	bool remakeC1 = true;
-	bool remakeC2 = true;
-
-	if (graph.at(e.v1).at(e.v2) == 0)
-	{
-		graph.at(e.v1).erase(e.v2);
-		if (graph.at(e.v1).size() == 0)
-		{
-			graph.erase(e.v1);
-			removeFromCluster(e.v1);
-			remakeC1 = false;
-		}
-	}
-
-	if (e.v1 != e.v2)
-	{
-		if (graph.at(e.v2).at(e.v1) == 0)
-		{
-			graph.at(e.v2).erase(e.v1);
-			if (graph.at(e.v2).size() == 0)
-			{
-				graph.erase(e.v2);
-				removeFromCluster(e.v2);
-				remakeC2 = false;
-			}
-		}
-	}
-
-	if (remakeC1 && remakeC2)
-	{
-		if (e.v1 != e.v2)
-		{
-			RemakeClusters(FindClusterIndex(e.v1), FindClusterIndex(e.v2));
-		}
-		else
-		{
-			int i = FindClusterIndex(e.v1);
-			RemakeClusters(i, i);
-		}
-	}
-	else
-	{
-		if (remakeC1)
-		{
-			int i = FindClusterIndex(e.v1);
-			RemakeClusters(i,i);
-		}
-		else if (remakeC2)
-		{
-			int i = FindClusterIndex(e.v2);
-			RemakeClusters(i, i);
-		}
-	}
-	*/
+	// Removes an edge from the graph
+	// When vertices become orphaned by this action, remove them as well
+	// Clusters with removed vertices are reclustered by first isolating each
+	//   vertex in its own cluster and them merging them with each connected
+	//   edge that is still in the graph.
+	
+	data->RemoveEdge(e);
+	
+	if (!data->HasEdgesWith(e.v1))
+		data->RemoveVertex(e.v1);
+	
+	if (!data->HasEdgesWith(e.v2))
+		data->RemoveVertex(e.v2);
 }
 
 //------------------------------------------------------------------------------
@@ -528,7 +307,8 @@ Vertices GraphManager::FindCluster(vertex u)
 	clusterid i = FindClusterIndex(u);
 	
 	Vertices vs;
-	for (ClusterPool::iterator it = data->pool.begin(); it != data->pool.end(); ++it)
+	ClusterPool::iterator it;
+	for (it = data->pool.begin(); it != data->pool.end(); ++it)
 		if (*it->second == i)
 			vs.push_back(it->first);
 	return vs;
