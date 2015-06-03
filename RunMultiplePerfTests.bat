e20f9967cb139420236ec4ac188ee196d115b106
Michiel Fortuin <michiel.fortuin@gmail.com>
2015-06-03 12:02:10 +0200

    Add rule start to the batch script Plots can now be rendered with log scale on the x axis

--- RunMultiplePerfTests.bat
+++ RunMultiplePerfTests.bat
@@ -8,9 +8,9 @@ echo Input,StructuralSampler,Metis,>> "%cluster_output_file%"
 
 rem run the tests
 rem higher cluster sizes
-CALL RunPerfTest %cluster_output_file% 1 100 %*
-CALL RunPerfTest %cluster_output_file% 1 20 %*
-CALL RunPerfTest %cluster_output_file% 1 10 %*
-CALL RunPerfTest %cluster_output_file% 1 2 %*
-CALL RunPerfTest %cluster_output_file% 1 1 %*
-CALL RunPerfTest %cluster_output_file% 5 1 %*
+CALL RunPerfTest %cluster_output_file% 1 100 0.01 %*
+CALL RunPerfTest %cluster_output_file% 1 20 0.05 %*
+CALL RunPerfTest %cluster_output_file% 1 10 0.1 %*
+CALL RunPerfTest %cluster_output_file% 1 2 0.5 %*
+CALL RunPerfTest %cluster_output_file% 1 1 1 %*
+CALL RunPerfTest %cluster_output_file% 5 1 5 %*
--- RunPerfTest.bat
+++ RunPerfTest.bat
@@ -4,15 +4,17 @@ rem run the tests
 set cluster_output_file=%1
 set cluster_queries=%2
 set cluster_updates=%3
+set cluster_rule_value=%4
+shift
 shift
 shift
 shift
 
 rem echo|set /p=%cluster_queries%:%cluster_updates%,>> "%cluster_output_file%"
-echo|set /p=%cluster_queries%,>> "%cluster_output_file%"
+echo|set /p=%cluster_rule_value%,>> "%cluster_output_file%"
 echo "%cluster_application%" %* -CA StructuralSampler 100 -CT Throughput %cluster_queries% %cluster_updates% -CO "%cluster_output_file%"
 "%cluster_application%" %* -CA StructuralSampler 100 -CT Throughput %cluster_queries% %cluster_updates% -CO "%cluster_output_file%"
-rem echo|set /p=,>> "%cluster_output_file%"
-rem echo "%cluster_application%" %* -CA Metis 100 -CT Throughput %cluster_queries% %cluster_updates% -CO "%cluster_output_file%"
-rem "%cluster_application%" %* -CA Metis 100 -CT Throughput %cluster_queries% %cluster_updates% -CO "%cluster_output_file%"
+echo|set /p=,>> "%cluster_output_file%"
+echo "%cluster_application%" %* -CA Metis 100 -CT Throughput %cluster_queries% %cluster_updates% -CO "%cluster_output_file%"
+"%cluster_application%" %* -CA Metis 100 -CT Throughput %cluster_queries% %cluster_updates% -CO "%cluster_output_file%"
 echo , >> "%cluster_output_file%"
