212547153e9742dbbb58f79a885fe1e44f8c704e
FerryT <f.timmers@student.tue.nl>
2015-06-16 16:28:50 +0200

    Added class diagrams

---
+++ documents/diagrams/generate.bat
@@ -0,0 +1,13 @@
+dot -Tpng -O edgelist.gv
+dot -Tpng -O graph.gv
+dot -Tpng -O clustering.gv
+dot -Tpng -O timedgraph.gv
+dot -Tpng -O algorithms.gv
+dot -Tpng -O inputs.gv
+dot -Tpng -O stackinputs.gv
+dot -Tpng -O tests.gv
+dot -Tpng -O overview.gv
+del edgelist.gv.png
+del graph.gv.png
+del clustering.gv.png
+del timedgraph.gv.png
